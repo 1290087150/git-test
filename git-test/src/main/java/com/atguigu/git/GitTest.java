ec69400ca4e42a01016b5d55b58722dc33e4c407
1290087150 <1290087150@qq.com>
2022-01-12 14:16:08 +0800

    master commit

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello,git--fix");
         System.out.println("hello,git--fix-fix");
         System.out.println("hello,git4!");
+        System.out.println("master");
     }
 }
