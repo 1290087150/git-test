4cf865f27a6314008a494900be02916847b481cd
1290087150 <1290087150@qq.com>
2022-01-12 13:59:57 +0800

    third commit

--- git-test/src/main/java/com/atguigu/git/GitTest.java
+++ git-test/src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git--idea");
         System.out.println("hello,git--fix");
+        System.out.println("hello,git--fix-fix");
     }
 }
